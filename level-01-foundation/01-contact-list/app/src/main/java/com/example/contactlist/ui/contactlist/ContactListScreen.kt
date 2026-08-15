0ddfe3c75441bfbadb028878fe1fd386fa9749c2
sangwoo kim <banana205@gmail.com>
2026-08-15 11:23:09 -0400

    remove changes

--- level-01-foundation/01-contact-list/app/src/main/java/com/example/contactlist/ui/contactlist/ContactListScreen.kt
+++ level-01-foundation/01-contact-list/app/src/main/java/com/example/contactlist/ui/contactlist/ContactListScreen.kt
@@ -1,14 +1,11 @@
 package com.example.contactlist.ui.contactlist
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @Composable
 fun ContactListRoute(
   viewModel: ContactListViewModel,
   modifier: Modifier = Modifier,
 ) {
-  val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 }
