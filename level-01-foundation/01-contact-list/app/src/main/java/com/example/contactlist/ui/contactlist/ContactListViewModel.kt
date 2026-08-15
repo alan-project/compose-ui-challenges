4aec5ddf62cbfb98e52564f1bbc4b8ab45c09ac8
sangwoo kim <banana205@gmail.com>
2026-08-15 11:31:43 -0400

    reset changes

--- level-01-foundation/01-contact-list/app/src/main/java/com/example/contactlist/ui/contactlist/ContactListViewModel.kt
+++ level-01-foundation/01-contact-list/app/src/main/java/com/example/contactlist/ui/contactlist/ContactListViewModel.kt
@@ -1,13 +1,8 @@
 package com.example.contactlist.ui.contactlist
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.contactlist.data.model.Contact
 import com.example.contactlist.data.repository.ContactRepository
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 
 data class ContactListUiState(
     val contacts: List<Contact> = emptyList()
@@ -15,13 +10,4 @@ data class ContactListUiState(
 class ContactListViewModel(
   private val contactRepository: ContactRepository,
 ) : ViewModel() {
-
-    val uiState: StateFlow<ContactListUiState> =
-        contactRepository.contacts.map { it: List<Contact> ->
-            ContactListUiState(it)
-        }.stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = ContactListUiState()
-        )
 }
