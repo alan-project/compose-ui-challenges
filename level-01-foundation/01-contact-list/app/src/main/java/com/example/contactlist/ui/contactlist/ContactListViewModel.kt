09a5ec062560d7a82281a04485c5867cbea1a775
sangwoo kim <banana205@gmail.com>
2026-08-15 11:32:16 -0400

    reset changes

--- level-01-foundation/01-contact-list/app/src/main/java/com/example/contactlist/ui/contactlist/ContactListViewModel.kt
+++ level-01-foundation/01-contact-list/app/src/main/java/com/example/contactlist/ui/contactlist/ContactListViewModel.kt
@@ -1,13 +1,9 @@
 package com.example.contactlist.ui.contactlist
 
 import androidx.lifecycle.ViewModel
-import com.example.contactlist.data.model.Contact
 import com.example.contactlist.data.repository.ContactRepository
 
-data class ContactListUiState(
-    val contacts: List<Contact> = emptyList()
-)
 class ContactListViewModel(
-  private val contactRepository: ContactRepository,
+    private val contactRepository: ContactRepository,
 ) : ViewModel() {
 }
