bbc9761757838b7ed41c5e179c3f0c17d04cac51
sangwoo kim <banana205@gmail.com>
2026-08-15 11:16:16 -0400

    Use flow-based contact state

--- level-01-foundation/01-contact-list/app/src/main/java/com/example/contactlist/data/repository/InMemoryContactRepository.kt
+++ level-01-foundation/01-contact-list/app/src/main/java/com/example/contactlist/data/repository/InMemoryContactRepository.kt
@@ -2,15 +2,15 @@ package com.example.contactlist.data.repository
 
 import com.example.contactlist.data.mock.MockContacts
 import com.example.contactlist.data.model.Contact
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class InMemoryContactRepository : ContactRepository {
   private val _contacts = MutableStateFlow(MockContacts.items)
 
-  override val contacts: StateFlow<List<Contact>> = _contacts.asStateFlow()
+  override val contacts: Flow<List<Contact>> = _contacts.asStateFlow()
 
   override fun toggleFavorite(contactId: Long) {
     _contacts.update { contacts ->
--- level-01-foundation/01-contact-list/app/src/main/java/com/example/contactlist/ui/contactlist/ContactListViewModel.kt
+++ level-01-foundation/01-contact-list/app/src/main/java/com/example/contactlist/ui/contactlist/ContactListViewModel.kt
@@ -1,9 +1,27 @@
 package com.example.contactlist.ui.contactlist
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.contactlist.data.model.Contact
 import com.example.contactlist.data.repository.ContactRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
+data class ContactListUiState(
+    val contacts: List<Contact> = emptyList()
+)
 class ContactListViewModel(
   private val contactRepository: ContactRepository,
 ) : ViewModel() {
+
+    val uiState: StateFlow<ContactListUiState> =
+        contactRepository.contacts.map { it: List<Contact> ->
+            ContactListUiState(it)
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = ContactListUiState()
+        )
 }
