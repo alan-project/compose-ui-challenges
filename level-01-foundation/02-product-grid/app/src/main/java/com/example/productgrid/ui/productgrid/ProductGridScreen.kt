08de899aaf516f44253184d91580565aaa519ef2
sangwoo kim <banana205@gmail.com>
2026-08-16 09:45:41 -0400

    added color

--- level-01-foundation/02-product-grid/app/src/main/java/com/example/productgrid/ui/productgrid/ProductGridScreen.kt
+++ level-01-foundation/02-product-grid/app/src/main/java/com/example/productgrid/ui/productgrid/ProductGridScreen.kt
@@ -2,6 +2,11 @@ package com.example.productgrid.ui.productgrid
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+private val FavoriteColor = Color(0xFFC45A64)
+private val RatingStarColor = Color(0xFFE2A51B)
+private val DiscountColor = Color(0xFFC62828)
 
 @Composable
 fun ProductGridRoute(
