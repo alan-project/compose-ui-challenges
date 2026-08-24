2a76b8adbac965080542fdb9c69a1499878b56db
alan-project <alan.project.canada@gmail.com>
2026-08-24 00:42:08 -0400

    Use immutable burger option collections

--- level-02-state-and-input/burger-builder/app/build.gradle.kts
+++ level-02-state-and-input/burger-builder/app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
   implementation(libs.androidx.compose.material3)
   implementation(libs.coil.compose)
   implementation(libs.coil.network.okhttp)
+  implementation(libs.kotlinx.collections.immutable)
   // Tooling
   debugImplementation(libs.androidx.compose.ui.tooling)
   // Instrumented tests
